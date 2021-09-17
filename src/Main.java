05134a55872f6fa0fa8704379ffba02f726bd99c
Ekaterina Miheeva <karamilllka@gmail.com>
2021-09-17 11:54:29 +0600

    lecture2-2

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
         int topUpBalance = 1500;
         int newBalance;
         int bonus = topUpBalance / 100;
-        if (topUpBalance < 1000) { newBalance = balance + topUpBalance;}
-                else { newBalance = balance + topUpBalance + bonus;}
-            System.out.println("Зачиленно: " + topUpBalance + "руб.");
-            System.out.println("Баланс: " + newBalance + "руб.");
+        if (topUpBalance < 1000) {
+            newBalance = balance + topUpBalance;
+        } else {
+            newBalance = balance + topUpBalance + bonus;
+        }
+        System.out.println("Зачиленно: " + topUpBalance + "руб.");
+        System.out.println("Баланс: " + newBalance + "руб.");
     }
 }
